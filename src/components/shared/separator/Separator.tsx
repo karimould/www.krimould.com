2ceff09ee016728f9079ec4997fad70921b5d5fc
karimould <karim_om@me.com>
2019-09-28 17:29:28 +0200

    fixed spaces between componente for mobile

--- src/components/shared/separator/Separator.tsx
+++ src/components/shared/separator/Separator.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 
 interface SeparatorProps {
-  distance: small | medium | large
+  distance: 'small' | 'medium' | 'large'
 }
 
 const Separator = ({ distance }: SeparatorProps): JSX.Element => {
-  const getClassName = (distance): string => {
+  const getClassName = (distance: string): string => {
     if (distance === 'small') {
       return 'mt-5'
     }
     if (distance === 'medium') {
-      return 'mt-32'
+      return 'mt-5 lg:mt-32'
     }
     if (distance === 'large') {
-      return 'mt-64'
+      return 'mt-32 lg:mt-64'
     }
+    return ''
   }
   return <div className={getClassName(distance)}></div>
 }
